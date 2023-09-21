7d615080d5994225872a639ff02228c45fe1d8c7
Anmitchell <avarynmitchell@gmail.com>
2023-09-20 20:03:52 -0400

    learning next.js, creating a blog

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+// `pages/_app.js`
+import '../styles/globals.scss'
+
+
+// global styles are being passed to all components
+// App component is the parent component to all page components
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+  }
